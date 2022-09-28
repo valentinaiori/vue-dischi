5014554a25f96e6553caa046059e32958159465d
valentinaiori <valentinaiori@live.it>
2022-09-28 21:46:06 +0200

    Add exercise complete

---
+++ src/components/AlbumContainerComponent.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container-card">
+        <div class="card" v-for="album in albums" :key="album.title" >
+        <div>
+            <img :src="album.poster">
+        </div>
+        <p class="album-title">{{album.title}}</p>
+        <p class="author">{{album.author}}</p>
+        <p class="year">{{album.year}}</p>
+    </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'AlbumContainerComponent',
+
+    props: {
+        albums: Array,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .container-card{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: stretch;
+        flex-wrap: wrap;
+        width: 1000px;
+        text-align: center;
+        height: 100%;
+        padding-bottom: 26px;
+
+        img{
+            width: 130px;
+            height: 140px;
+            padding-top: 20px;
+        }
+
+        .card{
+
+            display: flex;
+            flex-direction: column;
+            width: calc(100% / 5);
+            flex-wrap: wrap;
+            width: 150px;   
+            flex-wrap: wrap;   
+            background-color: #2e3a46;
+            margin: 10px;
+        }
+
+        .album-title{
+            padding-top: 20px;
+            padding-bottom: 20px;
+        }
+
+        .author, .year{
+            color: grey;
+        }
+
+        .year{
+            font-size: 0.8rem;
+            padding-bottom: 10px;
+        }
+    }
+
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,37 +1,56 @@
 <template>
   <main>
     <SearchGenreComponent :options="genres" @selectedGenre="filterByGenre"/>
-    <div class="container-card">
-        <div class="card" v-for="album in albums" :key="album.title" >
-        <div>
-            <img :src="album.poster">
-        </div>
-        <p class="album-title">{{album.title}}</p>
-        <p class="author">{{album.author}}</p>
-        <p class="year">{{album.year}}</p>
-    </div>
-    </div>
+    <AlbumContainerComponent :albums="albumToDisplay"/>
   </main>
 </template>
 
 <script>
 import axios from 'axios';
 import SearchGenreComponent from './SearchGenreComponent.vue';
+import AlbumContainerComponent from './AlbumContainerComponent.vue';
+
 
 
 export default {
     name: 'MainComponent',
     components: {
-        SearchGenreComponent
+        SearchGenreComponent,
+        AlbumContainerComponent
     },
 
     data(){
         return {
             apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
             albums: [],
-            seletedGenre: '',
+            selectedGenre: '',
        }
     },
+
+    created(){
+        this.getAlbumData();
+    },
+
+    methods: {
+        getAlbumData(){
+            axios.get(this.apiUrl).then((response)=>{
+                if(this.isResponseOK(response)){
+                    console.log(response.data)
+                this.albums = response.data.response
+                }
+            })
+        },
+  
+        isResponseOK({status}){
+            return status === 200
+        },
+
+        filterByGenre(genre){
+            this.selectedGenre = genre;
+            console.log(this.selectedGenre)
+        },
+    }, 
+
     computed:{
         genres(){
             const array= [];
@@ -48,7 +67,7 @@ export default {
             console.log('ciao')
             const array = [];
             this.albums.forEach(album =>{
-                if(this.seletedGenre.length === 0 || this.selectedGenre === album.genre){
+                if(this.selectedGenre.length === 0 || this.selectedGenre === album.genre){
                     array.push(album);
                 }
             })
@@ -56,33 +75,6 @@ export default {
         }
 
     },
-    
-
-    created(){
-        this.getAlbumData();
-    },
-
-    methods: {
-        getAlbumData(){
-            axios.get(this.apiUrl).then((response)=>{
-                console.log(response);
-                console.log('gggggggggg');
-                if(this.isResponseOK(response)){
-                    console.log(response.data)
-                this.albums = response.data.response
-                }
-            })
-        },
-  
-        isResponseOK({status}){
-            return status === 200
-        },
-
-        filterByGenre(genre){
-            console.log('main received', genre);
-            this.selectedGenre = genre;
-        },
-    }, 
 }
 </script>
 
@@ -90,49 +82,5 @@ export default {
     main{
         background-color: #1e2d3b;
         color: white;
-    }
-
-    .container-card{
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: stretch;
-        flex-wrap: wrap;
-        width: 1000px;
-        text-align: center;
-        height: 100%;
-        padding-bottom: 26px;
-
-        img{
-            width: 130px;
-            height: 140px;
-            padding-top: 20px;
-        }
-
-        .card{
-
-            display: flex;
-            flex-direction: column;
-            width: calc(100% / 5);
-            flex-wrap: wrap;
-            width: 150px;   
-            flex-wrap: wrap;   
-            background-color: #2e3a46;
-            margin: 10px;
-        }
-
-        .album-title{
-            padding-top: 20px;
-            padding-bottom: 20px;
-        }
-
-        .author, .year{
-            color: grey;
-        }
-
-        .year{
-            font-size: 0.8rem;
-            padding-bottom: 10px;
-        }
     }
 </style>
